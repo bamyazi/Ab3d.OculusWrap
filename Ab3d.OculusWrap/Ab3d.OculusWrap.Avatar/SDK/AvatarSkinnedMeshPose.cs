3e2398a45207d2d1646e45c574be584e7ab00b6f
Andrej <Andrej@HOMEWIN10>
2017-11-13 00:10:14 +0100

    Added GetJointNames to AvatarSkinnedMeshPose

--- Ab3d.OculusWrap/Ab3d.OculusWrap.Avatar/SDK/AvatarSkinnedMeshPose.cs
+++ Ab3d.OculusWrap/Ab3d.OculusWrap.Avatar/SDK/AvatarSkinnedMeshPose.cs
@@ -7,15 +7,39 @@ using System.Threading.Tasks;
 
 namespace Ab3d.OculusWrap.Avatar
 {
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    [StructLayout(LayoutKind.Sequential)]
     public struct AvatarSkinnedMeshPose
     {
         public UInt32 JointCount;
+
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 64)]
-        public AvatarTransform[] JointTransforms;
+        public AvatarTransform[] JointTransform;
+
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 64)]
-        public int[] JointParents;      
-        //[MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.LPStr, SizeConst = 64)]
-        //public string[] JointNames;
+        public int[] JointParents;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 64)]
+        public IntPtr[] JointNamePtrs;
+
+        public string[] GetJointNames()
+        {
+            var jointNames = new string[JointCount];
+
+            for (int i = 0; i < JointCount; i++)
+            {
+                var jointNamePtr = JointNamePtrs[i];
+
+                string jointName;
+
+                if (jointNamePtr != IntPtr.Zero)
+                    jointName = Marshal.PtrToStringAnsi(jointNamePtr);
+                else
+                    jointName = null;
+
+                jointNames[i] = jointName;
+            }
+
+            return jointNames;
+        }
     }
 }
